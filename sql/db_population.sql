be0e280decd8e806699f36250cf467b444c4f206
guthriehayward <guthriehayward@gmail.com>
2019-09-16 23:21:23 -0700

    Added products to db_population.sql

--- sql/db_population.sql
+++ sql/db_population.sql
@@ -9,3 +9,11 @@ insert into customer(userId, firstName, lastName, email, phoneNumber, addressOne
 insert into product(productName, price)
   values
   ('MSI MPG X570 GAMING EDGE WIFI Gaming Motherboard AMD AM4 SATA 6Gb/s M.2 USB 3.2 Gen 2 HDMI ATX', 209.99);
+
+insert into product(productName, price)
+  values
+  ('Intel Core i7-8700K Coffee Lake 6-Core 3.7 GHz (4.7 GHz Turbo) LGA 1151 (300 Series) 95W BX80684I78700K Desktop Processor Intel UHD Graphics 630', 349.99);
+
+insert into product(productName, price)
+values
+('ASUS Dual GeForce RTX 2080 DirectX 12 DUAL-RTX2080-O8G-EVO 8GB 256-Bit GDDR6 PCI Express 3.0 HDCP Ready SLI Support Video Card', 629.99);
