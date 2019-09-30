15e6093b7a6bee6298d6c28bcb25abf8f36a91e9
guthriehayward <guthriehayward@gmail.com>
2019-09-29 19:03:50 -0700

    More Django practice, worked with admin page

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Product(models.Model):
     productName = models.CharField(max_length = 255)
     price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    description = models.TextField(default="")
 
     def __str__(self):
         return self.productName
