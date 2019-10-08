28d0b3991ad7db53eefceb1cf64079a0868d4d7f
guthriehayward <guthriehayward@gmail.com>
2019-10-08 08:38:31 -0700

    Added Django views

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
+def homepage(request):
+    return render(request=request,
+                  template_name='main/home.html',
+                  context={'products': Product.objects.all})
